ca271134cca24e587f3ae46302ab0caa5d2259c2
Vedanand <vvinny021@gmail.com>
2026-05-30 23:22:43 +0530

    auth router setup

--- service/auth/src/index.ts
+++ service/auth/src/index.ts
@@ -33,25 +33,29 @@ async function initDb() {
       )
       `;
 
-      await sql`
+    await sql`
     CREATE TABLE IF NOT EXISTS skills (
         skill_id SERIAL PRIMARY KEY,
         name VARCHAR(100) NOT NULL UNIQUE
     );
 `;
 
-await sql`
+    await sql`
     CREATE TABLE IF NOT EXISTS user_skills (
         user_id INTEGER NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
         skill_id INTEGER NOT NULL REFERENCES skills(skill_id) ON DELETE CASCADE,
         PRIMARY KEY (user_id, skill_id)
 );
 `;
+  console.log("✅ Database initialized successfully");
   } catch (error) {
     console.log("❌ Error initializing database:", error);
   }
 }
 
-app.listen(process.env.PORT , () => {
-  console.log("Auth service is running on http://localhost:${process.env.PORT}");
-});
+
+initDb().then(() =>{
+  app.listen(process.env.PORT , () => {
+    console.log(`Auth service is running on http://localhost:${process.env.PORT}`);
+  });
+})
