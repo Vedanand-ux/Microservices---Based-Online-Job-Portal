741d5f4371d1db6d5ca0a8c41e6dd837c1f37ff8
Vedanand <vvinny021@gmail.com>
2026-06-03 23:42:05 +0530

    register user setup

--- service/auth/src/controllers/auth.ts
+++ service/auth/src/controllers/auth.ts
@@ -1,7 +1,19 @@
+import { sql } from "../utils/db.js";
+import ErrorHandler from "../utils/errorHandler.js";
 import {TryCatch} from "../utils/TryCatch.js";
 
 export const registerUser = TryCatch(async (req, res) => {
-    const {email} = req.body;
+    const {name, email, password, phonenumber, role, bio} = req.body;
+
+    if(!name || !email || !password || !phonenumber || !role) {
+        throw new ErrorHandler(400,"All fields are required");
+    }
+
+    const existingUser = await sql`SELECT user_id FROM users WHERE email = ${email}`;
+
+    if(existingUser.length > 0) {
+        throw new ErrorHandler(400,"User already exists");
+    }
 
     res.json(email);
 });
