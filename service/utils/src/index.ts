fdb9de9e095b06118a683913d944859e891581d9
Vedanand <vvinny021@gmail.com>
2026-06-08 23:23:24 +0530

    exporess middleware

--- service/utils/src/index.ts
+++ service/utils/src/index.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import router from './routes.js';
+import routes from './routes.js';
 
 dotenv.config();
 
 const app = express();
 
-app.use("/api/utils",router);
+app.use(express.json({limit:'50mb'}));
+app.use(express.urlencoded({limit:'50mb',extended:true}));
+
+app.use("/api/utils",routes);
 
 app.listen(process.env.PORT,()=>{
   console.log(`Utils service is running on http://localhost:${process.env.PORT}`);
