24bcab251ed006b45e5a8c254f70477d57ad9113
Vedanand <vvinny021@gmail.com>
2026-06-08 23:20:44 +0530

    import routes

--- service/utils/src/index.ts
+++ service/utils/src/index.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import router from './routes.js';
+
 dotenv.config();
 
 const app = express();
+
+app.use("/api/utils",router);
 
 app.listen(process.env.PORT,()=>{
   console.log(`Utils service is running on http://localhost:${process.env.PORT}`);
