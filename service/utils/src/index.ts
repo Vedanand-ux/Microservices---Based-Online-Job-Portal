d1a2a3bd42f1d0688d5070c46ae39731bf1e19a0
Vedanand <vvinny021@gmail.com>
2026-06-09 23:30:02 +0530

    Introduce Cloudinary

--- service/utils/src/index.ts
+++ service/utils/src/index.ts
@@ -1,10 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import routes from './routes.js';
+import cors from 'cors';
+import { v2 as cloudinary } from "cloudinary";
 
 dotenv.config();
 
+cloudinary.config({
+    cloud_name: "dwxvgplcu",
+    api_key: "154711964926888",
+    api_secret: "Rj37zKjqlQHy5Tx9Q09RVv8xoFY",
+});
+
 const app = express();
+app.use(cors());
 
 app.use(express.json({limit:'50mb'}));
 app.use(express.urlencoded({limit:'50mb',extended:true}));
