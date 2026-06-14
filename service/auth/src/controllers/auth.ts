79d1bc0c8e4fa3abb8b8e25730bfb97be45f47f6
Vedanand <vvinny021@gmail.com>
2026-06-14 23:27:41 +0530

    Import axios

--- service/auth/src/controllers/auth.ts
+++ service/auth/src/controllers/auth.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import getBuffer from "../utils/buffer.js";
 import { sql } from "../utils/db.js";
 import ErrorHandler from "../utils/errorHandler.js";
@@ -36,12 +37,17 @@ export const registerUser = TryCatch(async (req, res) => {
     const fileBuffer = getBuffer(file)
 
     if(!fileBuffer || !fileBuffer.content) {
-        throw new ErrorHandler(500,"falied to generate file ");
+        throw new ErrorHandler(500,"falied to generate buffer ");
     }
 
+    const { data } = await axios.post(
+        `${process.env.UPLOAD_SERVICE}/api/utils/upload`,
+        { buffer: fileBuffer.content }
+    );
+
     const [user] =
-    await sql`INSERT INTO users (name, email, password, phone_number, role) VALUES
-    (${name}, ${email}, ${hashedPassword}, ${phone_number}, ${role}) RETURNING
+    await sql`INSERT INTO users (name, email, password, phone_number, role, bio, resume, resume_public_id) VALUES
+    (${name}, ${email}, ${hashedPassword}, ${phone_number}, ${role}, ${bio}, ${data.url}, ${data.public_id}) RETURNING
     user_id, name, email, phone_number, role, created_at`;
 }
 
